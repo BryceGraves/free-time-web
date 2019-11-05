46b65e0a56423fe7e8adc66917c59e700e58a91b
Bryce Graves <brycegraves97@gmail.com>
2019-11-04 20:31:41 -0800

    Project: refactor structure and mock new app layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a react component
+class App extends React.Component {
+  state = {};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidUpdate() {
+    console.log('My compontent re-rendered');
+  }
+
+  renderContent() {
+    return <div>Test</div>;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderContent()}
+      </div>
+    );
+  };
+}
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
